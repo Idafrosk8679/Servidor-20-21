d932d7b5a5f49b884bc167112e46cd3e4ac2b2a4
Idafrosk8679 <43534516+Idafrosk8679@users.noreply.github.com>
2020-09-15 09:31:00 +0200

    Mas 2

--- Inicio/oop.php
+++ Inicio/oop.php
@@ -6,11 +6,39 @@ class coche {
     public $marca="Maserati";
     public $modelo="MC20";
 
+    public function __construct($color,$marca,$modelo=""){
+        $this->color=$color;
+        $this->marca=$marca;
+        $this->modelo=$modelo;
+    }
+
+    public function setColor($color){
+        if ($color !== 'azul' ) {
+            $this->color=$color;
+        } else {
+            $this->color="No Valido";
+        }
+    }
+
+    public function getColor(){
+       return $this->color;
+    }
+
     public function mostrar(){
-        return $this->modelo." ".$this->marca." ".$this->color;
+        return $this->marca." ".$this->modelo." ".$this->color;
     }
 }
 
-$var= new coche();
+$var= new coche('Negro','Opel','Vectra');
+$var->setColor('Naranja');
+
 echo $var->mostrar();
+echo "<br>";
+
+$var2= new coche('Verde','Citroen','C3');
+$var2->setColor('Azul');
+
+echo $var2->mostrar();
+echo "<br>";
+echo $var->color;
 ?>
