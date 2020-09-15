3cd603f6da1fbc2074e9890027b291604603770f
Idafrosk8679 <43534516+Idafrosk8679@users.noreply.github.com>
2020-09-15 08:59:04 +0200

    mas

--- Inicio/oop.php
+++ Inicio/oop.php
@@ -1,6 +1,16 @@
 <?php 
 
-class MyClass {
+class coche {
 
+    public $color="Rojo";
+    public $marca="Maserati";
+    public $modelo="MC20";
+
+    public function mostrar(){
+        return $this->modelo." ".$this->marca." ".$this->color;
+    }
 }
+
+$var= new coche();
+echo $var->mostrar();
 ?>
